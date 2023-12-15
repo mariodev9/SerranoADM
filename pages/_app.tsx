de407cf961113f7740963050b1ac541be50500c7
LucianoMariotti <luciano.mariotti99@gmail.com>
2023-12-14 21:17:04 -0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import Head from 'next/head';
+// import Head from 'next/head';
 // import "@/styles/globals.css"
 
 export default function MyApp({
@@ -33,11 +33,11 @@ export default function MyApp({
 
   return (
     <>
-    <Head>
+    {/* <Head>
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com"  />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""  />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
-    </Head>
+    </Head> */}
     <ChakraProvider  theme={theme}>
       <Layout {...pageProps}>
         <Component {...pageProps} />
